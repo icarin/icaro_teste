c4d8d694c41cc1a7125dd38c0ccac7a444587cac
icaro vincius <icaro7988@gmail.com>
2024-05-29 20:15:02 -0300

    modificando print

--- icaro.py
+++ icaro.py
@@ -1,6 +1,1 @@
-nome=input("qual seu nome?")
-idade= str(input("qual a sua idade?"))
-    if idade >=18:
-        print(nome,"você é apto")
-    elif idade <18:
-        print(nome,"vocênão apto")
+print("ola")
